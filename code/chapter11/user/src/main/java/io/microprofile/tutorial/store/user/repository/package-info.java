3ff593004da76e5a1e2f0bfcdce97ce1c62bfa41
Tarun Telang <tarun.telang@gmail.com>
2025-06-11 18:18:56 +0530

    Revert "updating code for chapter 11"
    This reverts commit 5f55b80f51ff29e5bcf78083fbb87a8566f7b2cd.

---
+++ code/chapter11/user/src/main/java/io/microprofile/tutorial/store/user/repository/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Repository classes for the user management module.
+ * 
+ * This package contains classes responsible for data access and persistence.
+ */
+package io.microprofile.tutorial.store.user.repository;
