d4f4e778080fd457de29744e5d9ec98bf1945e41
Tarun Telang <tarun.telang@gmail.com>
2026-02-02 18:16:34 +0000

    Update chapter 11 content for clarity and enhance MicroProfile Rest Client 4.0 introduction; update pom.xml for Open Liberty version 3.12.0; adjust server.xml for MicroProfile and Jakarta EE platform versions; add no-args constructor to Product class; ensure README reflects current features.

--- code/chapter02/mp-ecomm-store/src/main/java/io/microprofile/tutorial/store/product/entity/Product.java
+++ code/chapter02/mp-ecomm-store/src/main/java/io/microprofile/tutorial/store/product/entity/Product.java
@@ -1,10 +1,12 @@
 package io.microprofile.tutorial.store.product.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Product {
     private Long id;
     private String name;
